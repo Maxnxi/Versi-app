394bc38c9054a51dace595c2797319d0443159f6
Mks <nisq@ya.ru>
2021-04-06 12:52:24 +0300

    Initial Commit

---
+++ versi-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  versi-app
+//
+//  Created by Maksim on 06.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
